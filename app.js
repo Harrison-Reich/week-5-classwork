835ad7dce58aaeb7a587160c2d8ce27985f28468
Harrison Reich <hreich1998@gmail.com>
2021-12-02 12:14:45 -0800

    updated node calculator

---
+++ app.js
@@ -0,0 +1,16 @@
+// addition
+const add = parseInt(process.argv[2]) + parseInt(process.argv[3])
+console.log(add)
+
+// subtraction
+const sub = parseInt(process.argv[2]) - parseInt(process.argv[3])
+console.log(sub)
+
+// mulitplication
+const mul = parseInt(process.argv[2]) * parseInt(process.argv[3])
+console.log(mul)
+
+// division
+const div = parseInt(process.argv[2]) / parseInt(process.argv[3])
+console.log(div)
+
