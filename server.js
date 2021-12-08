ba94e52aad712bf33c7401661199d26f69fa2265
Harrison Reich <hreich1998@gmail.com>
2021-12-08 14:21:32 -0800

    updated list

--- server.js
+++ server.js
@@ -3,11 +3,23 @@ const express = require('express')
 const app = express()
 
 app.get('/', (req, res) => {
-  res.send('<h1>Home Page</h1>')
+  res.send('<h1>Harrison Reich Local Server!</h1>')
 })
 
-app.get('/greeting', (req, res) => {
-  res.send('hi')
+app.get('/food', (req, res) => {
+  res.send('<h1>This is my foods page!</h1>')
+})
+
+app.get('/music', (req, res) => {
+  res.send('<h1>These are my favorite songs page!</h1>')
+})
+
+app.get('/games', (req, res) => {
+  res.send('<h1>This is my games page!</h1>')
+})
+
+app.get('/tvshow', (req, res) => {
+  res.send('<h1>This is my tv shows page!</h1>')
 })
 
 app.listen(3000)
