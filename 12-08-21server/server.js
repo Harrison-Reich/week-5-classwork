ee78c1fd1fc401e8143f8dc7c43cbd392b70e526
Harrison Reich <hreich1998@gmail.com>
2021-12-09 10:58:05 -0800

    added files and json

--- 12-08-21server/server.js
+++ 12-08-21server/server.js
@@ -1,25 +1,32 @@
 const express = require('express')
+const path = require('path')
 
 const app = express()
 
 app.get('/', (req, res) => {
-  res.send('<h1>Harrison Reich Local Server!</h1>')
-})
+  res.sendFile(path.join(__dirname, 'example', 'index.html'))
+//   res.send('<h1>Harrison Reich Local Server!</h1>')
+// })
 
 app.get('/food', (req, res) => {
-  res.send('pancakes, pizza, tacos, salmon')
+  res.sendFile(path.join(__dirname, 'example', 'food.html'))
+  // res.send('pancakes, pizza, tacos, salmon')
 })
 
 app.get('/music', (req, res) => {
-  res.send('Strait Outta Tottenham, Damn, Racks, Too Eazy')
-})
+  res.sendFile(path.join(__dirname, 'example', 'music.html'))
+//   res.send('Strait Outta Tottenham, Damn, Racks, Too Eazy')
+// })
 
 app.get('/games', (req, res) => {
-  res.send('Madden, League of Legends, Call of Duty')
-})
+  res.sendFile(path.join(__dirname, 'example', 'games.html'))
+//   res.send('Madden, League of Legends, Call of Duty')
+// })
 
 app.get('/tvshow', (req, res) => {
-  res.send('Arcane, The Witcher, Family Guy')
+  res.sendFile(path.join(__dirname, 'example', 'tvshow.html'))
+  // res.send('Arcane, The Witcher, Family Guy')
 })
 
 app.listen(3001)
+
