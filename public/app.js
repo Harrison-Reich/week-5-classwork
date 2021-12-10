38d0a88993b567d8363144500dab6c28dae278ac
Harrison Reich <hreich1998@gmail.com>
2021-12-10 10:49:38 -0800

    updated files

--- public/app.js
+++ public/app.js
@@ -1,46 +1,58 @@
-console.log('Hello World!')
-
-axios.get('/food')
+axios.get('/user')
   .then(res => {
-    const food = res.data
+    const user = res.data
 
-    document.getElementById('eat').innerHTML = `
-      <h1>${food.taste}</h1>
-      <h2>${food.quality}</h2>
+    document.getElementById('user').innerHTML = `
+      <h1>${user.name}</h1>
+      <h2>${user.email}</h2>
+      <h2>${user.username}</h2>
+      <h2>${user.phoneNumber}</h2>
+      <h2>${user.address}</h2>
     `
   })
   .catch(err => console.error(err))
 
-axios.get('/music')
-  .then(res => {
-    const music = res.data
+// axios.get('/food')
+//   .then(res => {
+//     const food = res.data
 
-    document.getElementById('sound').innerHTML = `
-      <h1>${music.Genre}</h1>
-      <h2>${music.City}</h2>
-    `
-  })
-  .catch(err => console.error(err))
+//     document.getElementById('eat').innerHTML = `
+//       <h1>${food.taste}</h1>
+//       <h2>${food.quality}</h2>
+//     `
+//   })
+//   .catch(err => console.error(err))
 
-axios.get('/games')
-  .then(res => {
-    const games = res.data
+// axios.get('/music')
+//   .then(res => {
+//     const music = res.data
 
-    document.getElementById('play').innerHTML = `
-      <h1>${games.name}</h1>
-      <h2>${games.team}</h2>
-    `
-  })
-  .catch(err => console.error(err))
+//     document.getElementById('sound').innerHTML = `
+//       <h1>${music.Genre}</h1>
+//       <h2>${music.City}</h2>
+//     `
+//   })
+//   .catch(err => console.error(err))
 
-axios.get('/tvshows')
-  .then(res => {
-    const tvshows = res.data
+// axios.get('/games')
+//   .then(res => {
+//     const games = res.data
 
-    document.getElementById('watch').innerHTML = `
-    <h1>${watch.channel}</h1>
-    <h2>${watch.genre}</h2>
-    <h3>${watch.duration}</h3>
-    `
-  })
-  .catch(err => console.error(err))
+//     document.getElementById('play').innerHTML = `
+//       <h1>${games.name}</h1>
+//       <h2>${games.team}</h2>
+//     `
+//   })
+//   .catch(err => console.error(err))
+
+// axios.get('/tvshows')
+//   .then(res => {
+//     const tvshows = res.data
+
+//     document.getElementById('watch').innerHTML = `
+//     <h1>${watch.channel}</h1>
+//     <h2>${watch.genre}</h2>
+//     <h3>${watch.duration}</h3>
+//     `
+//   })
+  // .catch(err => console.error(err))
--- server.js
+++ server.js
@@ -5,78 +5,93 @@ const { rsort } = require('semver')
 const app = express()
 
 // objects
-const home = {
-  name: 'Homepage',
-  purpose: 'Starting location to website'
+const user = {
+  name: 'Billy',
+  email:'billy1@gmail.com',
+  username: 'billy1',
+  phoneNumber:'9491234567',
+  address:'42 Seashore Wallaby Way, Sidney',
 }
-const eat = {
-  taste: 10,
-  quality: 'fresh'
-}
+// const home = {
+//   name: 'Homepage',
+//   purpose: 'Starting location to website'
+// }
+// const eat = {
+//   taste: 10,
+//   quality: 'fresh'
+// }
 
-const sound = {
-  Genre: 'rap',
-  City: 'Los Angeles'
-}
+// const sound = {
+//   Genre: 'rap',
+//   City: 'Los Angeles'
+// }
 
-const play = {
-  name: 'Madden',
-  team: 'Denver Broncos'
-}
+// const play = {
+//   name: 'Madden',
+//   team: 'Denver Broncos'
+// }
 
-const watch = {
-  channel: 49,
-  genre: 'comedy',
-  duration: 45
-}
+// const watch = {
+//   channel: 49,
+//   genre: 'comedy',
+//   duration: 45
+// }
 
 app.use(express.static(path.join(__dirname, 'public')))
 
-app.get('/food', (req, res) => {
-  res.sendFile(path.join(__dirname, 'public', 'food.html'))
-})
-
-app.get('/music', (req, res) => {
-  res.sendFile(path.join(__dirname, 'public', 'music.html'))
-})
-
-app.get('/games', (req, res) => {
-  res.sendFile(path.join(__dirname, 'public', 'games.html'))
-})
-
-app.get('/tvshows', (req, res) => {
-  res.sendFile(path.join(__dirname, 'public', 'tvshows.html'))
-})
-
 app.get('/', (req, res) => {
-  // res.json(home)
-  // res.sendFile(path.join(__dirname, 'example', 'index.html'))
-  //   res.send('<h1>Harrison Reich Local Server!</h1>')
+  res.sendFile(path.join(__dirname, 'public', 'index.html'))
 })
 
-app.get('/food', (req, res) => {
-  res.json(eat)
-  // res.sendFile(path.join(__dirname, 'example', 'food.html'))
-  // res.send('pancakes, pizza, tacos, salmon')
+app.get('/user', (req, res) => {
+  res.json(user)
 })
 
-app.get('/music', (req, res) => {
-  res.json(sound)
-  // res.sendFile(path.join(__dirname, 'example', 'music.html'))
-  //   res.send('Strait Outta Tottenham, Damn, Racks, Too Eazy')
-})
+// app.get('/food', (req, res) => {
+//   res.sendFile(path.join(__dirname, 'public', 'food.html'))
+// })
 
-app.get('/games', (req, res) => {
-  res.json(play)
-  // res.sendFile(path.join(__dirname, 'example', 'games.html'))
-  //   res.send('Madden, League of Legends, Call of Duty')
-})
+// app.get('/music', (req, res) => {
+//   res.sendFile(path.join(__dirname, 'public', 'music.html'))
+// })
 
-app.get('/tvshows', (req, res) => {
-  res.json(watch)
-  // res.sendFile(path.join(__dirname, 'example', 'tvshow.html'))
-  // res.send('Arcane, The Witcher, Family Guy')
-})
+// app.get('/games', (req, res) => {
+//   res.sendFile(path.join(__dirname, 'public', 'games.html'))
+// })
+
+// app.get('/tvshows', (req, res) => {
+//   res.sendFile(path.join(__dirname, 'public', 'tvshows.html'))
+// })
+
+// app.get('/', (req, res) => {
+//   res.json(user)
+//   // res.sendFile(path.join(__dirname, 'example', 'index.html'))
+//   //   res.send('<h1>Harrison Reich Local Server!</h1>')
+// })
+
+// app.get('/food', (req, res) => {
+//   res.json(eat)
+//   // res.sendFile(path.join(__dirname, 'example', 'food.html'))
+//   // res.send('pancakes, pizza, tacos, salmon')
+// })
+
+// app.get('/music', (req, res) => {
+//   res.json(sound)
+//   // res.sendFile(path.join(__dirname, 'example', 'music.html'))
+//   //   res.send('Strait Outta Tottenham, Damn, Racks, Too Eazy')
+// })
+
+// app.get('/games', (req, res) => {
+//   res.json(play)
+//   // res.sendFile(path.join(__dirname, 'example', 'games.html'))
+//   //   res.send('Madden, League of Legends, Call of Duty')
+// })
+
+// app.get('/tvshows', (req, res) => {
+//   res.json(watch)
+//   // res.sendFile(path.join(__dirname, 'example', 'tvshow.html'))
+//   // res.send('Arcane, The Witcher, Family Guy')
+// })
 
 app.listen(3001)
 
